271bb5bf13bf6d509998fd42dc1211d37f70af93
Ingemar Hjälmstad <iingemar@gmail.com>
2015-02-24 20:45:17 +0100

    Added html, css and js

--- project/urls.py
+++ project/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
+    url(r'^$', 'app.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
