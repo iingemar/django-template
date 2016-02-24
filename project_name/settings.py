14ac8c2c763a805a18d7266745b1f962f6d74f9c
iingemar <iingemar@gmail.com>
2016-02-24 16:41:11 +0100

    Change db to sqlite3

--- project_name/settings.py
+++ project_name/settings.py
@@ -78,6 +78,14 @@ WSGI_APPLICATION = 'project_name.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'database_name',
         'USER': 'user_name',
@@ -86,6 +94,7 @@ DATABASES = {
         'PORT': '',
     }
 }
+"""
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
